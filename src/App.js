ee211f2d7b51364dbfd93bcdd01b4fa8a2500d17
Gabriel Souza <gablgdario03@gmail.com>
2023-12-15 16:00:51 -0300

    RESGATANDO DANDOS DE UMA API

--- src/App.js
+++ src/App.js
@@ -1,23 +1,37 @@
-import logo from './logo.svg';
-import './App.css';
+//hooks
+import { useEffect, useState } from "react";
+
+//style
+import "./App.css";
 
 function App() {
+  const [products, setProducts] = useState([]);
+  const url = "http://localhost:3000/products";
+
+  //1-Regastando dados
+  useEffect(() => {
+    async function fetchData() {
+      const res = await fetch(url);
+
+      const data = await res.json();
+
+      setProducts(data);
+    }
+    fetchData();
+  }, []);
+
+  console.log(products);
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>Lista de produtos</h1>
+      <ul>
+        {products.map((product) => (
+          <li key={product.id}>
+            {product.name} - R${product.price}
+          </li>
+        ))}
+      </ul>
     </div>
   );
 }
